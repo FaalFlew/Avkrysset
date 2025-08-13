91c1a092a5fbfc975b66de6b233c5ea03e08466e
Majd Abbas <126261797+FaalFlew@users.noreply.github.com>
2025-08-14 00:04:18 +0200

    added auth controller

---
+++ Api/Controllers/AuthController.cs
@@ -0,0 +1,35 @@
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Api.DTOs.Auth;
+using Api.Features.Auth.Commands;
+
+namespace Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuthController : ControllerBase
+{
+    private readonly ISender _mediator;
+
+    public AuthController(ISender mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpPost("register")]
+    [ProducesResponseType(typeof(AuthResponse), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status409Conflict)]
+    public async Task<IActionResult> Register(RegisterRequest request)
+    {
+        var command = new RegisterCommand(
+            request.Email,
+            request.Password,
+            request.MigrationData
+        );
+
+        var result = await _mediator.Send(command);
+
+        return Ok(result);
+    }
+}
