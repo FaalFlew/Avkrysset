70c7fb6a7d4a94fa079c23a1f6e40f4c68d3ef97
Majd Abbas <126261797+FaalFlew@users.noreply.github.com>
2025-08-14 00:00:30 +0200

    added global exception handler

---
+++ Api/Middleware/GlobalExceptionHandler.cs
@@ -0,0 +1,69 @@
+using System.Net;
+using System.Text.Json;
+using FluentValidation;
+
+namespace Api.Middleware;
+
+public class GlobalExceptionHandler
+{
+    private readonly RequestDelegate _next;
+    private readonly ILogger<GlobalExceptionHandler> _logger;
+
+    public GlobalExceptionHandler(RequestDelegate next, ILogger<GlobalExceptionHandler> logger)
+    {
+        _next = next;
+        _logger = logger;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception ex)
+        {
+            await HandleExceptionAsync(context, ex);
+        }
+    }
+
+    private async Task HandleExceptionAsync(HttpContext context, Exception exception)
+    {
+        context.Response.ContentType = "application/json";
+        var response = context.Response;
+
+        var statusCode = (int)HttpStatusCode.InternalServerError;
+        var message = "An unexpected error has occurred. Please try again later.";
+        var errors = new List<string>();
+
+        switch (exception)
+        {
+            case InvalidOperationException ex when ex.Message.Contains("already exists"):
+                statusCode = (int)HttpStatusCode.Conflict; // 409
+                message = ex.Message;
+                break;
+
+            case ValidationException ex:
+                statusCode = (int)HttpStatusCode.BadRequest; // 400
+                message = "One or more validation errors occurred.";
+                errors.AddRange(ex.Errors.Select(e => e.ErrorMessage));
+                break;
+
+
+
+            default:
+                _logger.LogError(exception, "An unhandled exception has occurred: {Message}", exception.Message);
+                break;
+        }
+
+        context.Response.StatusCode = statusCode;
+
+        var errorResponse = new
+        {
+            message,
+            errors
+        };
+
+        await context.Response.WriteAsync(JsonSerializer.Serialize(errorResponse));
+    }
+}
